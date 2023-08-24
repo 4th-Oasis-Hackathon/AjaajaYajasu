ecd78782ae49baedcc5204b3328601478bf0486d
Jimin <coolcats6762@gmail.com>
2023-08-24 20:11:43 +0900

    Revert "new commit"

--- app/src/main/java/com/example/pioena/MainActivity.java
+++ app/src/main/java/com/example/pioena/MainActivity.java
@@ -1,15 +1,78 @@
 package com.example.pioena;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+    Fragment4 fragment4;
+    Fragment5 fragment5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.custom_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true); // customizing
+        actionBar.setDisplayShowTitleEnabled(true); // 제목 보이게
+        actionBar.setDisplayHomeAsUpEnabled(false); // 뒤로가기 버튼(true이면 생김)
+        ImageButton imageButton = findViewById(R.id.search);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_LONG).show();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                SearchFragment searchFragment = new SearchFragment();
+                transaction.replace(R.id.container, searchFragment);
+                transaction.commit();
+            }
+        });
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+        fragment4 = new Fragment4();
+        fragment5 = new Fragment5();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit(); // 처음에는 홈 화면
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(
+                item -> {
+                    int itemId = item.getItemId();
+                    if (itemId == R.id.tab1) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+                        return true;
+                    } else if (itemId == R.id.tab2) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+                        return true;
+                    } else if (itemId == R.id.tab3) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();
+                        return true;
+                    } else if (itemId == R.id.tab4) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment4).commit();
+                        return true;
+                    } else if (itemId == R.id.tab5) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment5).commit();
+                        return true;
+                    }
+                    return false;
+                }
+        );
     }
+
 }
