b57815bc0d8729ed0476c3585c120725bb690888
armyants531 <giantmathbox@gmail.com>
2023-08-25 00:22:12 +0900

    fix LoginFragment

--- app/src/main/java/com/example/pioena/LoginFragment.java
+++ app/src/main/java/com/example/pioena/LoginFragment.java
@@ -1,16 +1,73 @@
 package com.example.pioena;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class LoginFragment extends Fragment {
+    Context context;
+    com.example.pioena.OnTabItemSelectedListener listener;
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+        if (context instanceof com.example.pioena.OnTabItemSelectedListener) {
+            listener = (com.example.pioena.OnTabItemSelectedListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        if (context != null) {
+            context = null;
+            listener = null;
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.loginfragment, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.loginfragment, container, false);
+
+        initUI(rootView);
+
+        return rootView;
+    }
+
+    private void initUI(ViewGroup rootView) {
+        EditText editText1, editText2;
+        editText1 = rootView.findViewById(R.id.etEmail);
+        editText2 = rootView.findViewById(R.id.etPassword);
+
+        final String[] id = new String[1];
+        final String[] pw = new String[1];
+
+        //((MainActivity)getActivity()).onWindowFocusChanged(true);
+
+        Button LoginButton = rootView.findViewById(R.id.btnLogin);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id[0] = editText1.getText().toString();
+                pw[0] = editText2.getText().toString();
+                if (id[0].length() == 0) {
+                    Toast.makeText(getContext(), "ID를 다시 입력해주세요", Toast.LENGTH_LONG).show();
+                } else if (pw[0].length() == 0) {
+                    Toast.makeText(getContext(), "PW를 다시 입력해주세요", Toast.LENGTH_LONG).show();
+                } else {
+                    if (listener != null) {
+                        listener.onTabSelected(0);  // fragment1으로(홈) 전환
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pioena/MainActivity.java
+++ app/src/main/java/com/example/pioena/MainActivity.java
@@ -13,13 +13,14 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    LoginFragment loginFragment;
     Fragment1 fragment1;
     Fragment2 fragment2;
     Fragment3 fragment3;
     Fragment4 fragment4;
     Fragment5 fragment5;
 
-
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,14 +44,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        loginFragment = new LoginFragment();
         fragment1 = new Fragment1();
         fragment2 = new Fragment2();
         fragment3 = new Fragment3();
         fragment4 = new Fragment4();
         fragment5 = new Fragment5();
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit(); // 처음에는 홈 화면
-        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, loginFragment).commit(); // 처음에는 홈 화면
+        bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(
                 item -> {
                     int itemId = item.getItemId();
@@ -73,6 +75,19 @@ public class MainActivity extends AppCompatActivity {
                     return false;
                 }
         );
+    }
+    public void onTabSelected(int position) {
+        if (position == 0) {
+            bottomNavigation.setSelectedItemId(R.id.tab1);
+        } else if (position == 1) {
+            bottomNavigation.setSelectedItemId(R.id.tab2);
+        } else if (position == 2) {
+            bottomNavigation.setSelectedItemId(R.id.tab3);
+        } else if (position == 3) {
+            bottomNavigation.setSelectedItemId(R.id.tab4);
+        } else if (position == 4) {
+            bottomNavigation.setSelectedItemId(R.id.tab5);
+        }
     }
 
 }
