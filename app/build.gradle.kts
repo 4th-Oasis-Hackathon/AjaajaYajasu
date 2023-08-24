02eafd70216e76ccdaca1eab9b4cd219f1887d37
armyants531 <giantmathbox@gmail.com>
2023-08-25 02:34:29 +0900

    하단바 타이틀 안보이게

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     // Glide
     implementation("com.github.bumptech.glide:glide:4.11.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
-
+    implementation("com.google.android.material:material:1.0.0-alpha3")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
