15ce3720f9bf6f3c0ab60f6bbb08f569ef751b9a
armyants531 <giantmathbox@gmail.com>
2023-08-26 02:40:30 +0900

    24:20 Message.java 생성자 수정 필요

--- app/src/main/java/com/example/pioena/Fragment4.java
+++ app/src/main/java/com/example/pioena/Fragment4.java
@@ -1,9 +1,11 @@
 package com.example.pioena;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
--- app/src/main/java/com/example/pioena/MyFlowerChatFragment.java
+++ app/src/main/java/com/example/pioena/MyFlowerChatFragment.java
@@ -1,16 +1,85 @@
 package com.example.pioena;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyFlowerChatFragment extends Fragment {
+    RecyclerView recyclerView;
+    TextView welcomeTextView;
+    EditText messageEditText;
+    ImageButton sendButton;
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.myflowerchatfragment, container, false);
+        super.onCreate(savedInstanceState);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.myflowerchatfragment, container, false);
+        messageList = new ArrayList<>();
 
+        recyclerView = rootView.findViewById(R.id.recycler_view_ai);
+        messageEditText = rootView.findViewById(R.id.message_edit_text);
+        sendButton = rootView.findViewById(R.id.text_send_btn);
+
+        // set up recyclerview
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String question = messageEditText.getText().toString().trim(); // trim() : 공백 제거
+                //Toast.makeText(getActivity(), question, Toast.LENGTH_LONG).show();
+                addToChat(question, Message.SENT_BY_ME);
+            }
+        });
+
+        androidx.constraintlayout.widget.ConstraintLayout layout = rootView.findViewById(R.id.chatBackground);
+        layout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                hideKeyboard();
+                return false;
+            }
+        });
+        return rootView;
+    }
+    private void hideKeyboard()
+    {
+        if (getActivity() != null && getActivity().getCurrentFocus() != null)
+        {
+            // 프래그먼트이므로 getActivity() 사용
+            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    // 이게 맞나? // 23:00
+    void addToChat(String message, String sentBy) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message());
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
     }
 }
