62d64d001dd55eccf18cf9c27bbc4ddd02ccc27e
armyants531 <giantmathbox@gmail.com>
2023-08-31 03:13:57 +0900

    fininal?

---
+++ app/src/main/java/com/example/pioena/SplashActivity2.java
@@ -0,0 +1,24 @@
+package com.example.pioena;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity2 extends AppCompatActivity {
+    Handler handler = new Handler();
+    @Override
+    protected void onCreate(@Nullable Bundle SaveInstanceState) {
+        super.onCreate(SaveInstanceState);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+}
